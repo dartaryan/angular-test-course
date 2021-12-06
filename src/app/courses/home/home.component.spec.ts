3a69acce2ef9333bcd45bf5310789b2814695ee9
Ben <dartaryan@gmail.com>
2021-12-06 14:48:12 +0200

    33. fakeAsync in Practice - Fixing the Home component tests

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,13 +1,27 @@
-import { DebugElement } from "@angular/core";
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { setupCourses } from "../common/setup-test-data";
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  flushMicrotasks,
+  TestBed,
+  tick,
+  waitForAsync,
+} from "@angular/core/testing";
 import { CoursesModule } from "../courses.module";
-import { CoursesService } from "../services/courses.service";
+import { DebugElement } from "@angular/core";
+
 import { HomeComponent } from "./home.component";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { CoursesService } from "../services/courses.service";
+import { HttpClient } from "@angular/common/http";
+import { COURSES } from "../../../../server/db-data";
+import { setupCourses } from "../common/setup-test-data";
 import { By } from "@angular/platform-browser";
 import { of } from "rxjs";
-import { by } from "protractor";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { click } from "../common/test-utils";
 
 describe("HomeComponent", () => {
@@ -19,6 +33,7 @@ describe("HomeComponent", () => {
   const beginnerCourses = setupCourses().filter(
     (course) => course.category == "BEGINNER"
   );
+
   const advancedCourses = setupCourses().filter(
     (course) => course.category == "ADVANCED"
   );
@@ -28,6 +43,7 @@ describe("HomeComponent", () => {
       const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
         "findAllCourses",
       ]);
+
       TestBed.configureTestingModule({
         imports: [CoursesModule, NoopAnimationsModule],
         providers: [{ provide: CoursesService, useValue: coursesServiceSpy }],
@@ -37,7 +53,7 @@ describe("HomeComponent", () => {
           fixture = TestBed.createComponent(HomeComponent);
           component = fixture.componentInstance;
           el = fixture.debugElement;
-          coursesService = TestBed.inject(CoursesService);
+          coursesService = TestBed.get(CoursesService);
         });
     })
   );
@@ -48,41 +64,89 @@ describe("HomeComponent", () => {
 
   it("should display only beginner courses", () => {
     coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+
     fixture.detectChanges();
+
     const tabs = el.queryAll(By.css(".mat-tab-label"));
+
     expect(tabs.length).toBe(1, "Unexpected number of tabs found");
   });
 
   it("should display only advanced courses", () => {
     coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
     fixture.detectChanges();
+
     const tabs = el.queryAll(By.css(".mat-tab-label"));
+
     expect(tabs.length).toBe(1, "Unexpected number of tabs found");
   });
 
   it("should display both tabs", () => {
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
     fixture.detectChanges();
+
     const tabs = el.queryAll(By.css(".mat-tab-label"));
+
     expect(tabs.length).toBe(2, "Expected to find 2 tabs");
   });
 
-  it("should display advanced courses when tab clicked", (done: DoneFn) => {
+  it("should display advanced courses when tab clicked - fakeAsync", fakeAsync(() => {
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
     fixture.detectChanges();
+
     const tabs = el.queryAll(By.css(".mat-tab-label"));
+
     click(tabs[1]);
+
     fixture.detectChanges();
-    setTimeout(() => {
-      const cardTitles = el.queryAll(By.css(".mat-card-title"));
-      expect(cardTitles.length).toBeGreaterThan(
-        0,
-        "Could not find card titles"
-      );
-      expect(cardTitles[0].nativeElement.textContent).toContain(
-        "Angular Security Course"
-      );
-      done();
-    }, 500);
-  });
+
+    flush();
+
+    const cardTitles = el.queryAll(
+      By.css(".mat-tab-body-active .mat-card-title")
+    );
+
+    console.log(cardTitles);
+
+    expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
+
+    expect(cardTitles[0].nativeElement.textContent).toContain(
+      "Angular Security Course"
+    );
+  }));
+
+  it(
+    "should display advanced courses when tab clicked - async",
+    waitForAsync(() => {
+      coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+      fixture.detectChanges();
+
+      const tabs = el.queryAll(By.css(".mat-tab-label"));
+
+      click(tabs[1]);
+
+      fixture.detectChanges();
+
+      fixture.whenStable().then(() => {
+        console.log("called whenStable() ");
+
+        const cardTitles = el.queryAll(
+          By.css(".mat-tab-body-active .mat-card-title")
+        );
+
+        expect(cardTitles.length).toBeGreaterThan(
+          0,
+          "Could not find card titles"
+        );
+
+        expect(cardTitles[0].nativeElement.textContent).toContain(
+          "Angular Security Course"
+        );
+      });
+    })
+  );
 });
